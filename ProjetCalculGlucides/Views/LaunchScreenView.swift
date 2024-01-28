1b40f0af5f6d6d9dacd069045c4ea2c4efb1642e
Cédric GILLOT <cedric.gillot@orange.fr>
2024-01-28 11:44:45 +0100

    Modification du LaunchScreen pour ajout d'un bouton de validation pour avoir lu l'avertissement

--- ProjetCalculGlucides/Views/LaunchScreenView.swift
+++ ProjetCalculGlucides/Views/LaunchScreenView.swift
@@ -9,39 +9,37 @@ import SwiftUI
 
 struct LaunchScreenView: View {
     
-    @State var timeIsElapsed = false
+    @State var buttonIsPressed = false
     
     var body: some View {
-        if timeIsElapsed {
+        if buttonIsPressed {
             MainView()
-        }
-        else {
-            ZStack {
-//                Image("launchImage")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fill)
-//                    .cornerRadius(20)
-                VStack {
-                    Text("GluciCalc")
-                        .font(.largeTitle)
-                        .bold()
-                        .foregroundStyle(.red)
-                        .padding(50)
-                    Text("Cette application est conçue pour offrir une assistance dans le calcul des doses d'insuline et vise à compléter, et non à remplacer, les directives et conseils fournis par les professionnels de santé. Elle ne doit pas être utilisée comme unique outil de décision pour la gestion de votre traitement du diabète. Veuillez toujours consulter un médecin ou un professionnel de santé qualifié pour des conseils médicaux personnalisés. L'utilisation de cette application se fait sous votre propre responsabilité et ne saurait engager notre responsabilité en cas de problèmes de santé ou d'autres complications.")
-                        .font(.title2)
-                        .foregroundStyle(.red)
-                        .padding(.horizontal, 5)
-                    Spacer()
-                }
+        } else {
+            VStack {
+                Text("GluciCalc")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.red)
+                    .padding(.bottom, 50)
+                Text("Cette application est conçue pour offrir une assistance dans le calcul des doses d'insuline et vise à compléter, et non à remplacer, les directives et conseils fournis par les professionnels de santé. Elle ne doit pas être utilisée comme unique outil de décision pour la gestion de votre traitement du diabète. Veuillez toujours consulter un médecin ou un professionnel de santé qualifié pour des conseils médicaux personnalisés. L'utilisation de cette application se fait sous votre propre responsabilité et ne saurait engager notre responsabilité en cas de problèmes de santé ou d'autres complications.")
+                    .font(.title2)
+                    .foregroundStyle(.red)
+                    .padding(.horizontal, 5)
+                Spacer()
+                Button(action: {
+                    buttonIsPressed = true
+                }, label: {
+                    ZStack {
+                        Capsule()
+                            .frame(width: 200, height: 60)
+                            .foregroundColor(.green)
+                        Text("J'ai compris")
+                            .font(.title2)
+                            .foregroundStyle(.primary)
+                    }
+                })
             }
             .padding()
-            .onAppear{
-                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                    withAnimation {
-                        timeIsElapsed = true
-                    }
-                }
-            }
         }
     }
 }
